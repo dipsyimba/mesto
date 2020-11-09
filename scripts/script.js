1ea525a1a6adfbee4ce3ecf4a7a50dc4238fcd2e
Azat Salimov <legendkris@yandex.ru>
2020-11-09 20:46:21 +0500

    template

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 
-let btnEditProfile = document.querySelector('.profile__edit-button');
-let popupEditProfile = document.querySelector('.popup');
-let btnClose = document.querySelector('.popup__btn-close');
-let nameInput = document.querySelector(".popup__input_profile_name");
-let jobInput = document.querySelector(".popup__input_profile_occupation");
-let name = document.querySelector(".profile__name");
-let occupation = document.querySelector(".profile__occupation");
-let formElement = document.querySelector(".popup__form");
+const btnEditProfile = document.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup_opened');
+const btnClose = document.querySelector('.popup__btn-close');
+const nameInput = document.querySelector(".popup__input_profile_name");
+const jobInput = document.querySelector(".popup__input_profile_occupation");
+const name = document.querySelector(".profile__name");
+const occupation = document.querySelector(".profile__occupation");
+const formElement = document.querySelector(".popup__form");
 
 
 function openEditProfile() {
